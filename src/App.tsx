f4593afa540a050be98ac180ff36ae08ba554241
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2025-06-24 14:59:50 +0200

    Change everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "./card/base.css";
+import "./card/rare.css";
+import { clamp, round } from "./helpers/math.ts";
+import card from "/card.png";
+
+function interact(event: React.PointerEvent<HTMLDivElement>) {
+  const el = event.target as HTMLElement; // cast to HTMLElement
+  const rect = el.getBoundingClientRect(); // get element's current size/position
+  const absolute = {
+    x: event.clientX - rect.left, // get mouse position from left
+    y: event.clientY - rect.top, // get mouse position from right
+  };
+  const percent = {
+    x: clamp(round((100 / rect.width) * absolute.x)),
+    y: clamp(round((100 / rect.height) * absolute.y)),
+  };
+  const norm = {
+    x: percent.x / 100 - 0.5,
+    y: percent.y / 100 - 0.5,
+  };
+  const deg = {
+    x: norm.y * 50,
+    y: -norm.x * 25,
+  };
+
+  const dynamicStyle = {
+    "--pointer-x": `${percent.x}%`,
+    "--pointer-y": `${percent.y}%`,
+    "--rotate-x": `${deg.x}deg`,
+    "--rotate-y": `${deg.y}deg`,
+  };
+
+  // Apply dynamicStyle to root
+  for (const [key, value] of Object.entries(dynamicStyle)) {
+    document.documentElement.style.setProperty(key, value);
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div
+        className="card"
+        style={{ height: "80vh", position: "relative" }}
+        onPointerMove={interact}
+      >
+        <img src={card} alt="Vite logo" style={{ height: "100%" }} />
+        <div className="shine"></div>
+        <div className="glare"></div>
       </div>
-      <h1>Vite + React (NOT 14 TEST) </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
