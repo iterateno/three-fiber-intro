bfb184af263979a6740678e6050f7e347985f2d7
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2025-12-03 19:55:44 +0100

    Add intro three fiber

---
+++ src/Scene.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Html,
+  OrbitControls,
+  Plane,
+  Sphere,
+  useAnimations,
+  useGLTF,
+} from "@react-three/drei";
+import { Canvas, MeshProps } from "@react-three/fiber";
+import { FC, useEffect, useState } from "react";
+
+const Cat: FC<MeshProps> = ({ scale, position }) => {
+  const [selectedAnimation, setSelectedAnimation] = useState("Wait");
+  const cat = useGLTF("/Cat.glb");
+  const animations = useAnimations(cat.animations, cat.scene);
+
+  useEffect(() => {
+    const action = animations.actions[selectedAnimation];
+    action?.reset().fadeIn(0.5).play();
+    return () => {
+      action?.fadeOut(0.5);
+    };
+  }, [selectedAnimation]);
+
+  return (
+    <>
+      <primitive object={cat.scene} scale={scale} position={position} />
+      <Html position={[0, 1.6, 0]} center>
+        <select
+          style={{
+            color: "white",
+            border: "none",
+            borderBottom: "solid 1px white",
+            fontSize: "1.625rem",
+            backgroundColor: "transparent",
+          }}
+          value={selectedAnimation}
+          onChange={(e) => setSelectedAnimation(e.target.value)}
+        >
+          <option value="Wait">Wait</option>
+          <option value="Walk">Walk</option>
+          <option value="Run">Run</option>
+        </select>
+      </Html>
+    </>
+  );
+};
+
+const Scene = () => (
+  <Canvas camera={{ position: [0, 2, 8], fov: 50 }}>
+    <color args={["#0c1e28"]} attach="background" />
+    <directionalLight position={[1, 1, 2]} intensity={6} />
+    <ambientLight intensity={0.5} />
+    <OrbitControls enableDamping dampingFactor={0.05} />
+
+    <Plane scale={10} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+      <meshStandardMaterial color="lightGrey" />
+    </Plane>
+
+    <Box position={[2, 0.5, -1]}>
+      <meshStandardMaterial color="red" />
+    </Box>
+
+    <Sphere scale={0.5} position={[-2, 0.5, -1]}>
+      <meshStandardMaterial color="blue" />
+    </Sphere>
+
+    <Cat scale={0.4} position={[0, 0, 0.5]} />
+  </Canvas>
+);
+
+export default Scene;
